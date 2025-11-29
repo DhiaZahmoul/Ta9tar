9cd331f7af0369e1129d498479a74ac8cf55bc8f
DhiaZahmoul <zahmouldhia@gmail.com>
2025-11-29 14:55:49 +0100

    fixed strating script

--- frontend/src/components/admin/actionsList.jsx
+++ frontend/src/components/admin/actionsList.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import CircularGallery from '../animated/list'
 const ActionsList = () => {
+    const getAllUsers = () => {
+        token = localStorage.getItem('token');
+        fetch('http://localhost:5000/api/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        .then(res => res.ok ? res.json() : []);
+        
+    }
+    const getAllChats=()=>{
+        token = localStorage.getItem('token');
+        fetch('http://localhost:5000/api/chats', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        .then(res => res.ok ? res.json() : []);
+    }
     const items = [
         { image: '/useIcon.png', text: 'Users' },
         { image: '/blog.png', text: 'Blogs' },
